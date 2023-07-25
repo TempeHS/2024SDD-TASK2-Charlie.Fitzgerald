bcf52d7d76378766fb63f26f3bf3c6b2594b5b65
Charlie <136764960+CharlieFitzgerald@users.noreply.github.com>
2023-07-25 22:45:09 +1000

    minor changes and lots of experimenting
    experimented a lot in terms of code, still havent gotten the squares to change, might look into https://codepen.io/chris_is_phillips/pen/jbJzKQ and redo code to fit pre existing code to get a general idea.

--- script.js
+++ script.js
@@ -44,41 +44,9 @@ function gameLoop (playerClicked) {
 	// convert random number outputs into a value that can be assigned into a square
 	// assign outputs to squares 
 	
-	// if (randomNumber == 1) {
-	// 	randomNumber == document.getElementById(<button type="button" value="1" class="button button1">-</button>) == "sq1";
-	// }
-	
-	// if (randomNumber == 2) {
-	// 	randomNumber == document.getElementById(<button type="button" value="2" class="button button2">-</button>) == "sq2";
-	// }
-	
-	// if (randomNumber == 3) {
-	// 	randomNumber == document.getElementById(<button type="button" value="3" class="button button3">-</button>) == "sq3";
-	// }
-	
-	// if (randomNumber == 4) {
-	// 	randomNumber == document.getElementById(<button type="button" value="4" class="button button4">-</button>) == "sq4";
-	// }
-	
-	// if (randomNumber == 5) {
-	// 	randomNumber == document.getElementById(<button type="button" value="5" class="button button5">-</button>) == "sq5";
-	// }
-	
-	// if (randomNumber == 6) {
-	// 	randomNumber == document.getElementById(<button type="button" value="6" class="button button6">-</button>) == "sq6";
-	// }
-	
-	// if (randomNumber == 7) {
-	// 	randomNumber == document.getElementById(<button type="button" value="7" class="button button7">-</button>) == "sq7";
-	// }
-	
-	// if (randomNumber == 8) {
-	// 	randomNumber == document.getElementById(<button type="button" value="8" class="button button8">-</button>) == "sq8";
-	// }
-	
-	// if (randomNumber == 9) {
-	// 	randomNumber == document.getElementById(<button type="button" value="9" class="button button9">-</button>) == "sq9";
-	// }
+	if (randomNumber == 1) {
+		randomNumber == document.getElementById;
+	}
 
 	const computerChoice = randomNumber //for debug only
 	equalityCheck (playerClicked, computerChoice);
@@ -89,6 +57,9 @@ function generateRandomNumber () {
 	return result;
 }
 
+
+// Logic part of the game
+
 function equalityCheck (playerChoice, computerChoice){
 	// perform an equality check, update scores and output a string using concatenation that gives user feedback 
 
@@ -97,7 +68,7 @@ function equalityCheck (playerChoice, computerChoice){
 	
 	let playerTip = 0;
 
-	// if (playerChoice == )
+	// if (playerChoice == 
 	
 		// if (playerChoice == "Rock" && computerChoice == 2) {
 		// 	playerTip = "Paper beats Rock! Computer Wins!"
